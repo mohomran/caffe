2e9c53b8633ae35cd7b94d627c2b3dd29569ea72
Mohamed Omran <mohamed.omran@gmail.com>
2014-09-19 22:26:44 +0200

    corrected paths for cifar10 demo

--- examples/cifar10/train_quick.sh
+++ examples/cifar10/train_quick.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-TOOLS=../../build/tools
+TOOLS=build/tools
 
-$TOOLS/caffe.bin train --solver=cifar10_quick_solver.prototxt
+$TOOLS/caffe.bin train --solver=examples/cifar10/cifar10_quick_solver.prototxt
 
 # reduce learning rate by fctor of 10 after 8 epochs
 $TOOLS/caffe.bin train \
-  --solver=cifar10_quick_solver_lr1.prototxt \
-  --snapshot=cifar10_quick_iter_4000.solverstate
+  --solver=examples/cifar10/cifar10_quick_solver_lr1.prototxt \
+  --snapshot=examples/cifar10/cifar10_quick_iter_4000.solverstate
